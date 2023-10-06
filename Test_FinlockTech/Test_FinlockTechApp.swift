eb89ebf1519c908162b8830d3dbeab8ee4269a0e
shivamchawla <shivamc.2002@gmail.com>
2023-10-06 23:53:23 +0530

    Initial Commit

---
+++ Test_FinlockTech/Test_FinlockTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_FinlockTechApp.swift
+//  Test_FinlockTech
+//
+//  Created by Shivam Chawla on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_FinlockTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
